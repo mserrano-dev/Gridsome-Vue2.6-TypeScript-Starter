e23c591f9ce9ec635964635e77ccb7801637ad00
Mark Anthony Serrano <markanthony.serrano2@gmail.com>
2020-10-01 06:35:39 -0700

    Setup svg, google analytics in gridsome config sample. Add breakpoints to tailwind config. Set typescript config to es2020.

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,6 +1,36 @@
 module.exports = {
   siteName: 'Mark Anthony Serrano Portfolio Website',
+  icon: 'static/assets/favicon.png',
+  chainWebpack: config => {
+    const svgRule = config.module.rule('svg');
+    svgRule.uses.clear();
+    svgRule
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader')
+      .options({
+        svgo: {
+          plugins: [
+            { cleanupIDs: false }
+          ]
+        }
+      });
+  },
+  templates: {
+    Projects: [
+      // {
+      //   path: '/projects/:slug',
+      //   component: './src/templates/singleProject.vue'
+      // }
+    ],
+  },
   plugins: [
+    // Analytics
+    {
+      use: '@gridsome/plugin-google-analytics',
+      options: {
+        id: process.env.GOOGLE_ANALYTICS,
+      }
+    },
     // Dev Experience
     {
       use: 'gridsome-plugin-typescript',
@@ -23,31 +53,11 @@ module.exports = {
         perPage: 100,
         concurrent: 10,
         customEndpoints: [
-          {
-            typeName: "Projects",
-            route: 'static/projects',
-            normalize: true,
-          },
-          {
-            typeName: "Header",
-            route: 'static/nav-header',
-            normalize: true,
-          },
-          {
-            typeName: "Sidebar",
-            route: 'static/nav-sidebar',
-            normalize: true,
-          },
-          {
-            typeName: "Footer",
-            route: 'static/nav-footer',
-            normalize: true,
-          },
-          {
-            typeName: "Mobile",
-            route: 'static/nav-mobile',
-            normalize: true,
-          },
+          // {
+          //   typeName: "Projects",
+          //   route: 'static/projects',
+          //   normalize: true,
+          // },
         ]
       }
     }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,20 @@
 module.exports = {
   purge: {
-    enabled: true,
+    enabled: process.env.NODE_ENV === 'production',
     content: [
       './src/**/*.html',
       './src/**/*.vue',
       './src/**/*.jsx',
     ]
   },
-  theme: {},
+  theme: {
+    screens: {
+      'sm': '320px', // iphone4 = 320x480
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+    }
+  },
   variants: {},
   plugins: [],
 }
